57a6e3ae0b9fae3ac51e06af4d054e644dba9a67
Viktor Didukh <vvdiduh@gmail.com>
2022-10-19 20:47:43 +0300

    add vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,12 @@
 import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
 
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
