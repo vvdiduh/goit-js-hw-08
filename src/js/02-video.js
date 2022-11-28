a8953cca6cd0056f5e9db64d0c85415d04f5bc25
vvdiduh <vvdiduh@gmail.com>
2022-11-28 16:20:27 +0200

    finish 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,31 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
 const TIME_PLAY = 'videoplayer-current-time';
 
+const saveTime = localStorage.getItem('TIME_PLAY');
+const currentTime = JSON.parse(saveTime);
+
 const finishTime = data => {
-    // localStorage.setItem(TIME_PLAY, JSON.stringify(data))
+    localStorage.setItem('TIME_PLAY', JSON.stringify(data.seconds))
 }
 
-player.on('timeupdate', finishTime);
+player.on('timeupdate', throttle(finishTime, 1000));
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = currentTime;
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
     
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+const refs = {
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+    form: document.querySelector('.feedback-form')
+}
 
+refs.form.addEventListener = ('submit', onFormSubmit);
+refs.email.addEventListener = ('input', onEmailInput);
+refs.message.addEventListener = ('input', onMessageInput);
+
+function onFormSubmit(evt){
+    evt.preventDefault();
+}
+
+function onEmailInput(evt) {
+    const value = evt.currentTarget.value;
+    console.log(value);
+}
+
+function onMessageInput(evt) {
+    const value = evt.currentTarget.value;
+    console.log(value);
+}
