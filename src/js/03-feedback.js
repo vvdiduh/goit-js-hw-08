d973470a42a4c5f5c4388e46c4137abf265f8e6d
Viktor Didukh <vvdiduh@gmail.com>
2022-11-28 23:55:09 +0200

    Finish task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,21 +3,37 @@ const refs = {
     message: document.querySelector('.feedback-form textarea'),
     form: document.querySelector('.feedback-form')
 }
+const FORM_VALUE = 'feedback-form-state';
+const formValue = {
+    email: '',
+    message: ''
+};
+const parsedValue = JSON.parse(localStorage.getItem('FORM_VALUE'));
 
-refs.form.addEventListener = ('submit', onFormSubmit);
-refs.email.addEventListener = ('input', onEmailInput);
-refs.message.addEventListener = ('input', onMessageInput);
+refs.form.addEventListener('submit', onFormSubmit);
+refs.email.addEventListener('input', onEmailInput);
+refs.message.addEventListener('input', onMessageInput);
+
+savedValue(parsedValue);
 
 function onFormSubmit(evt){
     evt.preventDefault();
+    localStorage.removeItem('FORM_VALUE');
+    evt.currentTarget.reset()
 }
 
 function onEmailInput(evt) {
-    const value = evt.currentTarget.value;
-    console.log(value);
+    formValue.email = evt.currentTarget.value;
+    localStorage.setItem('FORM_VALUE', JSON.stringify(formValue))
 }
 
 function onMessageInput(evt) {
-    const value = evt.currentTarget.value;
-    console.log(value);
+    formValue.message = evt.currentTarget.value;
+    localStorage.setItem('FORM_VALUE', JSON.stringify(formValue))
+}
+
+function savedValue(parsedValue) {
+    const { email, message } = parsedValue;
+    refs.email.value = email;
+    refs.message.value = message;
 }
